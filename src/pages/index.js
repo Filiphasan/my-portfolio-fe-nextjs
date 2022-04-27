87c49d2f929327a7c1511c68c639df8810b0a305
Hasan Erdal <hasaerda@hotmail.com>
2022-04-27 13:40:14 +0300

    second

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react"
 import get from "../services";
 
-export default function Home() {
-  const [data, setData] = useState({})
-  useEffect(() => {
-    get('aboutme/public')
-  }, [])
-
-
+export default function Home({data}) {
   return (
     <h1 className="text-3xl font-bold underline">
       {JSON.stringify(data)}
     </h1>
   )
+}
+
+export async function getServerSideProps(context) {
+  const data = await get('aboutme/public');
+  return {
+    props: { data },
+  };
 }
--- src/services/index.js
+++ src/services/index.js
@@ -9,17 +9,27 @@ const baseUrl = publicRuntimeConfig.baseApiUrl;
 axios.defaults.baseURL = baseUrl;
 
 const get = (endpoint) => {
-    const data = axios.get(endpoint).then(
-        res => {
-            console.log(res.data);
-            return res.data;     
-        }
-    )
+    return axios.get(endpoint, getHeader()).then(handleResponse)
 }
 
+const post = (endpoint, data) => {
+    return axios.post(endpoint, data, getHeader()).then(handleResponse)
+}
+
+const handleResponse = (response) => {
+    const data = response.data;
+    if (data && data.status === 'success'){
+        return data.data
+    }else if (data && data.status === 'error'){
+        // API tarafından anlamlı hata mesajı dönmesi
+        console.log(data.message);
+    }else{
+        return Promise.reject("Beklenmeyen Bir Hata Oluştu!")
+    }
+}
 
 const getHeader = (contentType = "application/json") => {
-    const token = sessionStorage.getItem(constantsVariables.tokenSessionKey);
+    const token = ""; // Tokeni cookieden çekmenin yolunu bulacağım
     if (token) {
         return {
             'Authorization': `Bearer ${token}`,
